1552bba94c1a6e26407511feb111fde112daea80
LakshaySain <73537063+LakshaySain@users.noreply.github.com>
2020-11-24 21:36:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+var thickness,wall;
+var speed,weight,bullet;
+
+
+function setup() {
+  createCanvas(1600,400);
+  
+speed = random(55,90)
+weight = random(400,1500)
+
+thickness = random(22,83)
+
+  bullet= createSprite(50,200,50,5)
+  bullet.velocityX = speed;
+  bullet.shapeColor= color(255);
+
+
+
+ 
+  wall= createSprite(1200, 200, thickness, height/2)
+  wall.shapeColor=color(80,80,80)
+  
+}
+
+function draw() {
+ background(0)
+  drawSprites();
+}
